b69d60650d91b0a75b1f549c763e57296e4bab4b
addefault <90063257+addefault@users.noreply.github.com>
2021-09-11 17:38:58 +0400

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -132,7 +132,7 @@ $('.items > li').click(function() {
 		let iframeWidth = 510;
 		if($(window).height() < 640)
 			iframeWidth = 400;
-		$('.iteminner').html('<video src="design.mp4" controls autoplay></video>').show();
+		$('.iteminner').html('<video src="design.mp4" autoplay></video>').show();
 	}
 });
 $('.plans > div').click(function() {
